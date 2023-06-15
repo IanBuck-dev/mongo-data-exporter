f068493d1ce0d16827cdc4046aee2cde121cd79a
Ian <ian@awork.io>
2023-06-15 09:09:09 +0200

    Add name to function

--- function_app.py
+++ function_app.py
@@ -9,8 +9,8 @@ from azure.storage.blob import BlobServiceClient
 
 app = func.FunctionApp()
 
-@app.schedule(schedule="0 0 8 * * *", arg_name="myTimer", run_on_startup=True,
-              use_monitor=False) 
+@app.function_name(name="myTimer")
+@app.schedule(schedule="0 0 8 * * *", arg_name="myTimer", run_on_startup=False, use_monitor=True) 
 def MongoSensorDataExporter(myTimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
@@ -25,10 +25,12 @@ def MongoSensorDataExporter(myTimer: func.TimerRequest) -> None:
     now = datetime.datetime.now()
     entries = collection.find({'Timestamp': {'$lt': now}})
 
+    logging.info('Starting to download mongodb entries.')
     df = pd.DataFrame(list(entries))
     csv_data = df.to_csv(index=False)
 
     # Clean up mongoDb readings
+    logging.info('Starting to delete mongodb entries.')
     collection.delete_many({})
 
     # Upload to blob storage
@@ -39,6 +41,7 @@ def MongoSensorDataExporter(myTimer: func.TimerRequest) -> None:
 
     container_name = 'sensor-data'
     container_client = blob_service_client.get_container_client(container=container_name)
+    logging.info('Starting to upload csv data.')
     container_client.upload_blob(name=filename, data=csv_data, overwrite=True)
 
     logging.info('Mongo exporter timer trigger function ran at %s', utc_timestamp)
