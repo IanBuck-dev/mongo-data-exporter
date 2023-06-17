fd00e992421ddfac5e760ca9e0e5e454ae3ecfb3
Ian <ian@awork.io>
2023-06-17 11:44:19 +0200

    Add try except

--- function_app.py
+++ function_app.py
@@ -3,7 +3,6 @@ import logging
 import azure.functions as func
 from pymongo import MongoClient
 import os
-import datetime
 import pandas as pd
 from azure.storage.blob import BlobServiceClient
 
@@ -14,34 +13,37 @@ app = func.FunctionApp()
 def MongoSensorDataExporter(myTimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
+    try:
+        # Connect to mongo db and download latest sensor readings.
+        mongoUserPw = os.environ.get('SENSOR_MONGODB_PW')
 
-    # Connect to mongo db and download latest sensor readings.
-    mongoUserPw = os.environ.get('SENSOR_MONGODB_PW')
+        client = MongoClient(f'mongodb+srv://mongouser:{mongoUserPw}@cluster.gbiuwau.mongodb.net/?retryWrites=true&w=majority')
+        db = client['test']
+        collection = db['sensor_readings_timeseries']
 
-    client = MongoClient(f'mongodb+srv://mongouser:{mongoUserPw}@cluster.gbiuwau.mongodb.net/?retryWrites=true&w=majority')
-    db = client['test']
-    collection = db['sensor_readings_timeseries']
+        now = datetime.datetime.now()
+        entries = collection.find({'Timestamp': {'$lt': now}})
 
-    now = datetime.datetime.now()
-    entries = collection.find({'Timestamp': {'$lt': now}})
+        logging.info('Starting to download mongodb entries.')
+        df = pd.DataFrame(list(entries))
+        csv_data = df.to_csv(index=False)
 
-    logging.info('Starting to download mongodb entries.')
-    df = pd.DataFrame(list(entries))
-    csv_data = df.to_csv(index=False)
+        # Clean up mongoDb readings
+        logging.info('Starting to delete mongodb entries.')
+        collection.delete_many({})
 
-    # Clean up mongoDb readings
-    logging.info('Starting to delete mongodb entries.')
-    collection.delete_many({})
+        # Upload to blob storage
+        filename = f'sensor_readings_{now.day}_{now.month}_{now.year}.csv'
 
-    # Upload to blob storage
-    filename = f'sensor_readings_{now.day}_{now.month}_{now.year}.csv'
+        connection_str =  os.environ.get('AzureWebJobsStorage')
+        blob_service_client = BlobServiceClient.from_connection_string(conn_str=connection_str)
 
-    connection_str =  os.environ.get('AzureWebJobsStorage')
-    blob_service_client = BlobServiceClient.from_connection_string(conn_str=connection_str)
+        container_name = 'sensor-data'
+        container_client = blob_service_client.get_container_client(container=container_name)
+        logging.info('Starting to upload csv data.')
+        container_client.upload_blob(name=filename, data=csv_data, overwrite=True)
 
-    container_name = 'sensor-data'
-    container_client = blob_service_client.get_container_client(container=container_name)
-    logging.info('Starting to upload csv data.')
-    container_client.upload_blob(name=filename, data=csv_data, overwrite=True)
+        logging.info('Mongo exporter timer trigger function ran at %s', utc_timestamp)
 
-    logging.info('Mongo exporter timer trigger function ran at %s', utc_timestamp)
+    except Exception as e:
+        logging.exception(e)
